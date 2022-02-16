eba822a563009d1650d2e76e715ca5b1cd95fac8
vinicius jesus <vinicius.je99@gmail.com>
2022-02-16 18:22:05 -0300

    navbar design and mobile design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,16 @@
 import React from 'react' 
+import './NavBar.css'
 
 export default function NavBar(){
     return(
-        <nav>
-            <button>teste</button>
-            <button>teste</button>
-            <button>teste</button>
+        <nav className="nav-container">
+            <h3>Filter</h3>
+            <div className="nav-btn-container">
+                <button className="nav-btn active">$125.000</button>
+                <button className="nav-btn">$200.000</button>
+                <button className="nav-btn">$250.000</button>
+                <button className="nav-btn">$300.000</button>
+            </div>
         </nav>
     )
 }
